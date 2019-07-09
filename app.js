8c8d333c9fb71a043dacc8aba93962fa46180ab5
Jahanzaib5 <50393893+Jahanzaib5@users.noreply.github.com>
2019-07-09 15:18:42 +0600

    processing

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(
         return Event.find()
           .then(events => {
               return events.map(event => {
-                  return {...event._doc, _id: event._doc._id.toString() }
+                  return {...event._doc, _id: event.id }
               })
           })
           .catch(err => {
@@ -68,7 +68,7 @@ app.use(
           .save()
           .then(result => {
             console.log(result);
-            return { ...result._doc };
+            return { ...result._doc, _id: result._doc._id.toString()};
           })
           .catch(err => {
             console.log(err);
