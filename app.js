0f5b4cd8c6ca0f96be76d5356fb65a35f21907bd
Jahanzaib5 <50393893+Jahanzaib5@users.noreply.github.com>
2019-07-09 15:05:15 +0600

    processing

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
 
-
+const Event = require("./models/event");
 
 const app = express();
 
-const events = [];
-
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHttp({
+app.use(
+  "/graphql",
+  graphqlHttp({
     schema: buildSchema(`
 
         type Event{
@@ -45,39 +45,45 @@ app.use('/graphql', graphqlHttp({
         }
     `),
     rootValue: {
-        events: () => {
-            return events;
-        },
+      events: () => {
+        return events;
+      },
 
-        createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: args.eventInput.date
-            };
-            events.push(event);
-            return event;
-
-        }
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return {...result._doc};
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      }
     },
 
     graphiql: true
- })
+  })
 );
 
-
-mongoose.connect(
+mongoose
+  .connect(
     `mongodb+srv://${process.env.MONGO_USER}:${
-        process.env.MONGO_PASSWORD
-    }@cluster0-frii1.mongodb.net/test?retryWrites=true&w=majority`
-    )
-    .then(() => {
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err);
-    });
-
-
+      process.env.MONGO_PASSWORD
+    }@cluster0-frii1.mongodb.net/${
+      process.env.MONGO_DB
+    }?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
